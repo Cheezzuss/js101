6ca620440def84ae006efd620550434bdbaeb330
Jesus <j.martinh247@gmail.com>
2019-05-04 12:21:32 -0400

    add asnwers 1-5

--- exercises/level1.js
+++ exercises/level1.js
@@ -1,34 +1,41 @@
 function lvl1exercise1 () {
   // Declare a variable without instantiating it and return it!
-
+var example;
+return example;
 }
 
 function lvl1exercise2 () {
   // Declare and instantiate a number and return it
-
+var num = 4;
+return num;
 }
 
 function lvl1exercise3 () {
   // Declare and instantiate a floating point number that is not a whole number and return it
-
+var flt = 3.14;
+return flt;
 }
 
 function lvl1exercise4 () {
   // Declare and instantiate a string "Hello World!" and return it
-
+var str = "Hello World!";
+return str;
 }
 
 function lvl1exercise5 () {
   // Declare and instantiate an array containing the string "Hello World!" and the number 4 and return it
-
+var arr = ["Hello World!", 4];
+return arr;
 }
 
 function lvl1exercise6 () {
   // Declare and instantiate an object containing the key-value pairs key1 -> "Hello World!" and key2 -> 4, and return it
-
+var object = {key1:"Hello World!", key2: 4};
+return object;
 }
 
 function lvl1exercise7 () {
   // Declare and instantiate a boolean value 'false' and return it
-
+var test = false;
+return test;
 }
--- exercises/level6.js
+++ exercises/level6.js
@@ -1,13 +1,29 @@
 function lvl6exercise1 (num) {
   // Return 'hello' if num is 1, 'world' if num is 2, otherwise return 'bye'
+  if (num == 1){
+    return 'hello'
+  }
+  else if (num == 2){
+    return 'world'
+  }
+  else{
+    return 'bye'
+  } 
 }
 
 function lvl6exercise2 () {
   // Push 10 'hello' strings into the array using a for loop, then return it
-  var arr = []
+  var arr = ['hello']
+  for (var i = 0;i<10;i++){
+    return (arr[i].givenAnA())
+  }
 }
 
 function lvl6exercise3 () {
   // Empty this array using a while loop and return it
   var arr = ['hello', 'hello', 'hello', 'hello', 'hello', 'hello', 'hello', 'hello', 'hello', 'hello']
+  //while (true){
+    //arr.pop()
+  //}
 }
+
